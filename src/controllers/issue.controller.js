df2c5362ace25900ee1c82e1bb634fbcf3ec45a8
iamkaus <kaustubhsingh369@gmail.com>
2025-03-29 20:42:14 +0530

    updated: src\controllers\issue.controller.js

--- src/controllers/issue.controller.js
+++ src/controllers/issue.controller.js
@@ -1,4 +1,4 @@
-
+import { prisma } from '../config/prisma.client.js'
 
 /**
  * Create Issue Controller for Express.js
@@ -38,10 +38,77 @@ export const createIssue = async (req, res, next) => {
         if ( status && !issueStatus.includes(status.toUpperCase()) ) {
             return res.status(400).json(
                 {
+                    success: false,
                     error: 'Status must be one of: [OPEN, IN-PROGRESS, RESOLVED, CLOSED]'
                 }
             )
         }
+
+        const validPriorities = ['LOW', 'MEDIUM', 'HIGH', 'CRITICAL'];
+        if ( priority && !validPriorities.includes(priority.toUpperCase()) ) {
+            return res.status(400).json(
+                {
+                    success: false,
+                    error: 'Priority must be one of: [LOW, MEDIUM, HIGH, CRITICAL]'
+                }
+            )
+        }
+
+        if ( assigneeId ) {
+            const assignee = await prisma.user.findUnique({
+                where: {
+                    id: assigneeId
+                }
+            })
+
+            if ( !assignee ) {
+                return res.status(400).json(
+                    {
+                        success: false,
+                        error: `Assignee with id: ${assigneeId} not found`
+                    }
+                )
+            }
+        }
+
+        const creatorId = req.user.id
+
+        if ( !creatorId ) {
+            return res.status(400).json(
+                {
+                    success: false,
+                    error: 'Creator id not found'
+                }
+            )
+        }
+
+        const newIssue = await prisma.issue.create({
+            data: {
+                title: title,
+                description: description,
+                creatorId: creatorId,
+                ...(status && { status: status.toUpperCase() }),
+                ...(priority && { priority: priority.toUpperCase() }),
+                ...(assigneeId && { assigneeId: assigneeId }),
+            }
+        })
+
+        if ( !newIssue ) {
+            return res.status(400).json(
+                {
+                    success: false,
+                    error: 'Failed to create issue'
+                }
+            )
+        }
+
+        return res.status(201).json(
+            {
+                success: true,
+                message: 'Issue created successfully',
+                data: newIssue
+            }
+        )
 
     } catch ( error ) {
         next(error);
